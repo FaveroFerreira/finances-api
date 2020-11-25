de21fabfc639af7b916d245b017ad5871858fd82
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2020-11-25 20:49:04 -0300

    Ajustes Swagger

--- src/main/java/com/deliver/finances/config/SwaggerConfig.java
+++ src/main/java/com/deliver/finances/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.deliver.finances.config;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,13 +16,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.deliver.finances"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(new ApiInfoBuilder()
+                        .title("finances-api")
+                        .version("1.0.0")
+                        .contact(new Contact("Guilherme Favero Ferreira",
+                                "https://github.com/FaveroFerreira/finances-api",
+                                ""))
+                        .build())
+                .tags(new Tag("bills", "Criação e listagem de contas."))
+                .produces(Collections.singleton("application/json"))
+                .consumes(Collections.singleton("application/json"));
     }
 
 }
