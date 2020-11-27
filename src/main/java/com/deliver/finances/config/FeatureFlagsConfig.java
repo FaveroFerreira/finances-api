a133d9ec6ef879fd6bc664f04373c45861e12c1f
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2020-11-27 01:30:10 -0300

    add security with togglz

--- src/main/java/com/deliver/finances/config/FeatureFlagsConfig.java
+++ src/main/java/com/deliver/finances/config/FeatureFlagsConfig.java
@@ -4,16 +4,30 @@ import com.deliver.finances.config.flags.FeatureFlags;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.togglz.core.manager.EnumBasedFeatureProvider;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.repository.jdbc.JDBCStateRepository;
 import org.togglz.core.repository.util.DefaultMapSerializer;
 import org.togglz.core.spi.FeatureProvider;
+import org.togglz.core.user.FeatureUser;
+import org.togglz.core.user.SimpleFeatureUser;
+import org.togglz.core.user.UserProvider;
 
 @Configuration
 public class FeatureFlagsConfig {
 
     private static final String FEATURE_FLAG_TABLE = "FEATURE_FLAG_CONFIGURATION";
+
+    @Bean
+    public UserProvider userProvider() {
+        return new UserProvider() {
+            @Override
+            public FeatureUser getCurrentUser() {
+                return null;
+            }
+        };
+    }
 
     @Bean
     public FeatureProvider featureProvider() {
